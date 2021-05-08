adb5c899d06f0f4dfa60c3b4cd234397d1a98c3f
Ian Žonja <ianzonja@gmail.com>
2021-05-08 23:59:58 +0200

    added linter & fixed errors

--- src/abstractions/unit.abstract.ts
+++ src/abstractions/unit.abstract.ts
@@ -1,47 +1,46 @@
-import { Army } from "src/models/army.model";
-import { BattleGround } from "./battleground.interface";
+import { Army } from 'src/models/army.model';
+import { BattleGround } from './battleground.interface';
 
-export abstract class Unit{
+export abstract class Unit {
+  type: string;
+  health: number;
+  damage: number;
+  defence: number;
+  id: number;
+  isAlive = true;
 
-    type: string;
-    health: number;
-    damage: number;
-    defence: number;
-    id: number;
-    isAlive: boolean = true;
+  getType(): string {
+    return this.type;
+  }
 
-    getType(): string{
-        return this.type;
-    }
-    
-    getHealth(): number{
-        return this.health;
-    }
+  getHealth(): number {
+    return this.health;
+  }
 
-    getAttack(): number{
-        return this.damage;
-    }
+  getAttack(): number {
+    return this.damage;
+  }
 
-    getDefence(): number{
-        return this.defence;
-    }
+  getDefence(): number {
+    return this.defence;
+  }
 
-    addDamage(damage: number){
-        this.damage = this.damage + damage;
-        return this;
-    }
+  addDamage(damage: number) {
+    this.damage = this.damage + damage;
+    return this;
+  }
 
-    addHealth(health: number){
-        this.health = this.health + health;
-        return this;
-    }
+  addHealth(health: number) {
+    this.health = this.health + health;
+    return this;
+  }
 
-    addDefence(defence: number){
-        this.defence = this.defence + defence;
-        return this;
-    }
+  addDefence(defence: number) {
+    this.defence = this.defence + defence;
+    return this;
+  }
 
-    set setId(id: number){
-        this.id = id;
-    }
+  set setId(id: number) {
+    this.id = id;
+  }
 }
